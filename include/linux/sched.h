76d67568b1968c8d874b8f6e1b5d22c1ce75fe00
Aurora Maintainer <otavio@aurora>
2026-09-13 05:00:18 -0300

    SUSFS fc-adapted hooks + branding -Aurora

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1519,8 +1519,15 @@ struct task_struct {
 	ANDROID_KABI_RESERVE(3);
 	ANDROID_KABI_RESERVE(4);
 	ANDROID_KABI_RESERVE(5);
-	ANDROID_KABI_RESERVE(6);
+#if defined(CONFIG_KSU_SUSFS)
+	ANDROID_KABI_USE(6, u64 susfs_task_state);
 #else
+	ANDROID_KABI_RESERVE(6);
+#endif
+#else
+#if defined(CONFIG_KSU_SUSFS)
+	u64				susfs_task_state;
+#endif
 	struct mutex			futex_exit_mutex;
 #endif
 
@@ -1530,7 +1537,11 @@ struct task_struct {
 #else
 	ANDROID_KABI_RESERVE(7);
 #endif
+#if defined(CONFIG_KSU_SUSFS)
+	ANDROID_KABI_USE(8, u64 susfs_last_fake_mnt_id);
+#else
 	ANDROID_KABI_RESERVE(8);
+#endif
 
 	/*
 	 * New fields for task_struct should be added above here, so that
